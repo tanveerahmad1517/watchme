068a4c20b586c9ae55ace4f5df87f59cfb225398
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2017-12-02 15:00:40 -0500

    basic features added

--- watchme/web/views.py
+++ watchme/web/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    request.META['host'] = str(request.get_host())
+    
+    for k in request.META:
+        print(k + ': ' + str(request.META[k]))
+
+    return render(request, 'index.html', context={'data': request.META})
