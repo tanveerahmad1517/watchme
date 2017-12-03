a82b4346733b224bd9cf94c5042860910602b1c4
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2017-12-02 21:07:17 -0500

    added possible ip checking

--- watchme/web/models.py
+++ watchme/web/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Addrs(models.Model):
     ip = models.GenericIPAddressField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now=True)
--- watchme/web/views.py
+++ watchme/web/views.py
@@ -4,7 +4,22 @@ from web.models import *
 
 # Create your views here.
 def index(request):
+    context = dict()
+    context['new_guest'] = True
+    context['prev_visit'] = None
+
     ip = request.META['REMOTE_ADDR']
-    print(ip)
+    if Addrs.objects.filter(ip=ip).all():
+        context['new_guest'] = False
+        context['prev_visit'] = Addrs.objects.filter(ip=ip).all()[0].timestamp.strftime('%m %B %Y @ %-I:%M:%S%p')
+        
+        # update the timestamp
+        Addrs.objects.filter(ip=ip).all()[0].save()
+    else:
+        # save this 'user' to the database
+        Addrs(ip=ip).save()
+    
+    context['data'] = request.META
+
     print(request.session)
-    return render(request, 'index.html', context={'data': request.META})
+    return render(request, 'index.html', context=context)
