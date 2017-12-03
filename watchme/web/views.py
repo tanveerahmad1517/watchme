3f5010829ec401946f0b50997bf04e15355114aa
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2017-12-02 21:09:59 -0500

    forgot to print day

--- watchme/web/views.py
+++ watchme/web/views.py
@@ -11,7 +11,7 @@ def index(request):
     ip = request.META['REMOTE_ADDR']
     if Addrs.objects.filter(ip=ip).all():
         context['new_guest'] = False
-        context['prev_visit'] = Addrs.objects.filter(ip=ip).all()[0].timestamp.strftime('%m %B %Y @ %-I:%M:%S%p')
+        context['prev_visit'] = Addrs.objects.filter(ip=ip).all()[0].timestamp.strftime('%-d %B %Y @ %-I:%M:%S%p')
         
         # update the timestamp
         Addrs.objects.filter(ip=ip).all()[0].save()
